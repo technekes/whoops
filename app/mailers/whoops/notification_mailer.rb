1f3376e867304424369e87b93aa82c0a9caf4db2
John Allen <john@threedogconsulting.com>
2011-11-03 11:29:13 -0400

    added support for current url in mailer

--- app/mailers/whoops/notification_mailer.rb
+++ app/mailers/whoops/notification_mailer.rb
@@ -5,6 +5,9 @@ class Whoops::NotificationMailer < ActionMailer::Base
   def event_notification(event_group, *addresses)
     @event_group = event_group
     @addresses = addresses
+    # better option?
+    @host = "http://#{Rails.configuration.action_mailer.default_url_options[:host]}/"
+    
     mail(
       :to      => addresses.join(", "),
       :subject => "Whoops Notification | #{event_group.service}: #{event_group.environment}: #{event_group.message}"

d191ea849fd70ee1a8379a51374a96cd94877e90
John Allen <john@threedogconsulting.com>
2011-11-03 10:12:53 -0400

    moved mail from, troubleshooting sending mail via sendgrid and heroku

--- app/mailers/whoops/notification_mailer.rb
+++ app/mailers/whoops/notification_mailer.rb
@@ -1,9 +1,11 @@
 class Whoops::NotificationMailer < ActionMailer::Base
+  default :from => 'no-reply@example.com',
+          :return_path => 'system@example.com'
+
   def event_notification(event_group, *addresses)
     @event_group = event_group
     @addresses = addresses
     mail(
-      :from    => "no-reply@email.com",
       :to      => addresses.join(", "),
       :subject => "Whoops Notification | #{event_group.service}: #{event_group.environment}: #{event_group.message}"
     )
